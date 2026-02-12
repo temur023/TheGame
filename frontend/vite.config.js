ed262b12dab060621461478211b140e49f278830
Temurmalik <temurmalikirgashev@gmail.com>
2026-02-12 20:52:31 +0500

    Finish

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'determined-success-production-6aba.up.railway.app'
+    ],
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: true
+  }
 })
