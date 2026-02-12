76e6fb9856c59dcb61fa4cea4b22c28de3bf5761
Temurmalik <temurmalikirgashev@gmail.com>
2026-02-12 21:33:26 +0500

    Finish

---
+++ TheGame/Migrations/20260212163219_ChangeMatchPasswordToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheGame.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeMatchPasswordToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MatchPassword",
+                table: "Matches",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MatchPassword",
+                table: "Matches",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
