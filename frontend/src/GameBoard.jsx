71fe2dc26e8f679ac862f283f293dddef89c9bd8
Temurmalik <temurmalikirgashev@gmail.com>
2026-02-12 21:11:50 +0500

    Finish

--- frontend/src/GameBoard.jsx
+++ frontend/src/GameBoard.jsx
@@ -10,7 +10,7 @@ function GameBoard() {
     const [connection, setConnection] = useState(null);
     const [chatMessage, setChatMessage] = useState("");
     const [chatLog, setChatLog] = useState([]);
-    const api_url = "htpps://thegame-production-9b97.up.railway.app";
+    const api_url = "https://thegame-production-9b97.up.railway.app";
 
     const chatEndRef = useRef(null);
     const connectionRef = useRef(null);
